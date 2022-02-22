b6f43a697757846795634520f821ae86a11fe02c
M Gilang Januar <mgilangjanuar@gmail.com>
2022-02-22 09:10:14 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { program } from 'commander'
+import fs from 'fs'
+
+program
+  .name('teledrive-merger')
+  .version('0.1.0')
+  .description('Merges huge files')
+
+program
+  .option('-i, --input <files...>', 'Input all file parts')
+  .option('-o, --output <file>', 'Output file')
+  .action(async ({ input, output }) => {
+    console.log('Start...')
+    try {
+      fs.writeFileSync(output, Buffer.from([]))
+      input.reduce((_: Buffer, file: string, i: number) => {
+        fs.appendFileSync(output, fs.readFileSync(file))
+        console.log('Merging... ' + (i + 1) + '/' + input.length)
+      }, Buffer.from([]))
+      console.log('Done file saved in ' + output)
+    } catch (error) {
+      console.error(`Error: ${JSON.stringify(error)}`)
+    }
+  })
+
+program.parse()
